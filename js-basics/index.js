4f96e2c58b33c68d4daf409b24510c58974fe6da
MeShootIn <dmitriimishutin@gmail.com>
2022-10-17 04:13:01 +0500

    Add another JavaScript notes

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,18 +1,27 @@
 const { log } = require('./helpers');
 
+
 function debug(SomeClass) {
-    log(SomeClass);
+  log(SomeClass);
 
-    const someInstance = new SomeClass();
+  const someInstance = new SomeClass();
 
-    log(SomeClass.prototype);
-    log(someInstance.__proto__);
+  log(SomeClass.prototype);
+  log(someInstance.__proto__);
 
-    // log(typeof SomeClass); // function
-    // log(typeof someInstance); // object
-    log(someInstance instanceof Object); // true
+  // log(typeof SomeClass); // function
+  // log(typeof someInstance); // object
+  log(someInstance instanceof Object); // true
 
-    log();
+  log();
+}
+
+const Dima = {
+  greet: function () {
+    console.log('hello')
+  },
+  name: 'Dima',
+  age: 23,
 }
 
 // class A { };
